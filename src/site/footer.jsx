6519c1de39a14a12cbe3e016361c05ed9c8f0390
tjiptostevens <steveneonites@gmail.com>
2022-07-15 20:58:42 +0700

    add footer

---
+++ src/site/footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import '../assets/css/footer.css'
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div className="footer-container">
+          <div>
+            <ul>
+              <li>Audio and Subtitles</li>
+              <li>Media Center</li>
+              <li>Security</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Provision</li>
+              <li>Legal Provisions</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <button className="footer-button">Service Code</button>
+        </div>
+        <div className="copyright">
+          <small>&copy; 2022 Movies. All Right Reserved.</small>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer
--- src/site/home.jsx
+++ src/site/home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from './footer'
+import Navbar from './navbar'
 
 const Home = () => {
   return (
     <>
+      <Navbar />
       <div>Home Page</div>
+      <Footer />
     </>
   )
 }
