6d9ea97f8e0a5a671a468c28a125c22509160885
suryanergara1210 <ik.surya.negara@cs.unud.ac.id>
2022-07-22 00:47:26 +0800

    delete

--- src/containers/Login_app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Login from '../site/login'
-
-export default function Login_app() {
-  return (
-    <Login loginOrRegister={"login"} />
-
-  )
-}
--- src/containers/Register_app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Login from '../site/login'
-
-export default function Register_app() {
-  return (
-    <Login loginOrRegister={"register"} />
-
-  )
-}
